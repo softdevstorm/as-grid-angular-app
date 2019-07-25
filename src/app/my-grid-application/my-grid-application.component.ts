96b18ace126f42b1754666586b8721ae0c60228c
softdevstorm <softdevstorm@gmail.com>
2019-07-25 20:42:41 +0400

    update

--- src/app/my-grid-application/my-grid-application.component.ts
+++ src/app/my-grid-application/my-grid-application.component.ts
@@ -15,26 +15,60 @@ export class MyGridApplicationComponent implements OnInit {
     private gridOptions: GridOptions;
     private gridApi;
     private gridColumnApi;
-    private frameworkComponents;
+    private overlayLoadingTemplate;
+    // private overlayNoRowsTemplate;
+    private columnDefs;
     private rowData = [];
 
     title = 'grid app';
-
-    columnDefs = [
-        {headerName: '', field: 'thumbnails', sortable: true, filter: true},
-        {headerName: 'Published on', field: 'publishedAt', sortable: true, filter: true},
-        {headerName: 'Video Title', field: 'title', sortable: true, filter: true},
-        {headerName: 'Description', field: 'description', sortable: true, filter: true}
-    ];
 
     constructor(
         private http: HttpClient,
         private dataService: DataService
     ) {
+        this.columnDefs = [
+        {
+            headerName: '', 
+            field: 'thumbnails', 
+            sortable: true, 
+            filter: true
+        },
+        {
+            headerName: 'Published on', 
+            field: 'publishedAt', 
+            sortable: true, 
+            filter: true
+        },
+        {
+            headerName: 'Video Title', 
+            field: 'title', 
+            sortable: true, 
+            filter: true,
+        },
+        {
+            headerName: 'Description', 
+            field: 'description', 
+            sortable: true, 
+            filter: true
+        }
+    ];
+
+        this.overlayLoadingTemplate = '<span class="ag-overlay-loading-center">Please wait while your rows are loading</span>';
+        // this.overlayNoRowsTemplate = "<span style=\"padding: 10px; border: 2px solid #444; background: lightgoldenrodyellow;\">This is a custom 'no rows' overlay</span>";
     }
 
     onGridReady(params) {
         this.gridApi = params.api; // To access the grids API
+        this.gridColumnApi = params.columnApi;
+        this.gridApi.showLoadingOverlay();
+        this.loadData();
+    }
+
+    ngOnInit() {
+        
+    }
+
+    private loadData() {
         this.dataService.getData().subscribe(data => {
             let json_datas = data.items;
             for (var index in json_datas) {
@@ -44,13 +78,9 @@ export class MyGridApplicationComponent implements OnInit {
                 row['title'] = json_datas[index]['snippet']['title'];
                 row['description'] = json_datas[index]['snippet']['description'];
                 this.rowData.push(row);
-                this.gridApi.setRowData(this.rowData);
             }
+            this.gridApi.setRowData(this.rowData);
         })
-    }
-
-    ngOnInit() {
-        
     }
 
 }
