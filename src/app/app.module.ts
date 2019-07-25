8e78a91414f665899c2941a591010a066710888b
softdevstorm <softdevstorm@gmail.com>
2019-07-25 15:54:01 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgGridModule } from "ag-grid-angular";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyGridApplicationComponent } from './my-grid-application/my-grid-application.component';
+import { RedComponentComponent } from './red-component/red-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyGridApplicationComponent,
+    RedComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents(
+      [RedComponentComponent]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
