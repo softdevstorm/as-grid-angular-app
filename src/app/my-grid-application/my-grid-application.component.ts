dd0d5781584f761233c143d680ac23ce44591647
softdevstorm <softdevstorm@gmail.com>
2019-07-26 12:20:19 +0400

    update

--- src/app/my-grid-application/my-grid-application.component.ts
+++ src/app/my-grid-application/my-grid-application.component.ts
@@ -5,6 +5,7 @@ import { AgGridAngular } from 'ag-grid-angular';
 import { HttpClient } from '@angular/common/http';
 import { RedComponentComponent } from "../red-component/red-component.component";
 import { DataService } from "../@services/get-data.service";
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-my-grid-application',
@@ -17,13 +18,13 @@ export class MyGridApplicationComponent implements OnInit {
     private gridApi;
     private gridColumnApi;
     private overlayLoadingTemplate;
-    // private overlayNoRowsTemplate;
     private columnDefs;
-    private rowSelection;
-    private statusBar;
+    private defaultColDef;
+    private getRowHeight;
     private rowData = [];
 
     private recordCount = 0;
+    private selectedCount = 0;
 
     title = 'grid app';
 
@@ -33,61 +34,49 @@ export class MyGridApplicationComponent implements OnInit {
     ) {
         this.columnDefs = [
             {
-                headerName: '', 
-                field: '', 
-                headerCheckboxSelection: true,
-                checkboxSelection: true,
-                // filter: true
+                headerName: '',
+                field: 'id',
+                width: 50,
+                cellRenderer: this.customCheckBox,
+                autoHeight: true
             },
             {
                 headerName: '', 
-                field: 'thumbnails', 
-                // sortable: true, 
-                // filter: true
+                field: 'thumbnails',
+                cellRenderer: this.tuhmbnails,
+                autoHeight: true
             },
             {
                 headerName: 'Published on', 
-                field: 'publishedAt', 
-                // sortable: true, 
-                // filter: true
+                field: 'publishedAt',
+                autoHeight: true
             },
             {
                 headerName: 'Video Title', 
                 field: 'title', 
-                // sortable: true, 
-                // filter: true,
-                cellRenderer: this.customCellRendererFunc
+                cellRenderer: this.customCellRendererFunc,
+                autoHeight: true
             },
             {
                 headerName: 'Description', 
-                field: 'description', 
-                // sortable: true, 
-                // filter: true
+                field: 'description',
+                autoHeight: true
             }
         ];
 
-        this.rowSelection = "multiple";
+        this.defaultColDef = {
+            resizable: true
+        };
+
+        this.getRowHeight = function(params) {
+            if (params.node.level === 0) {
+                return 98;
+            } else {
+                return 25;
+            }
+        };
 
         this.overlayLoadingTemplate = '<span class="ag-overlay-loading-center">Please wait while your rows are loading</span>';
-        // this.overlayNoRowsTemplate = "<span style=\"padding: 10px; border: 2px solid #444; background: lightgoldenrodyellow;\">This is a custom 'no rows' overlay</span>";
-
-        // ag Grid statusbar
-        // this.statusBar = {
-        //     statusPanels: [{
-        //             statusPanel: "agTotalRowCountComponent",
-        //             align: "left"
-        //         },
-        //         {
-        //             statusPanel: "agFilteredRowCountComponent"
-        //         },
-        //         {
-        //             statusPanel: "agSelectedRowCountComponent"
-        //         },
-        //         {
-        //             statusPanel: "agAggregationComponent"
-        //         }
-        //     ]
-        // };
     }
 
     onGridReady(params) {
@@ -98,15 +87,69 @@ export class MyGridApplicationComponent implements OnInit {
     }
 
     ngOnInit() {
-        
+        let self = this;
+        setTimeout(() => {
+            $(document).ready(function() {
+                var checkboxHeaderColumn = $('.ag-header-cell-text')[0];
+                $(checkboxHeaderColumn).html(self.customHeaderCheckBox());
+            });
+        }, 700);
+
+        // toogle row checkbox
+        $(document)
+            .off("click", ".ag-custom-checkbox input[type='checkbox']")
+            .on("click", ".ag-custom-checkbox input[type='checkbox']", function() {
+                var checkboxId = $(this).prop('id');
+                var rowDataIndex = checkboxId.replace('checkbox', '');
+                rowDataIndex = parseInt(rowDataIndex);
+                 if ($(this).prop('checked') == true) {
+                    self.selectedCount += 1;
+                    self.rowData[rowDataIndex]['id']['checkbox'] = true;
+                } else if ($(this).prop('checked') == false) {
+                    if (self.selectedCount > 0) {
+                        self.rowData[rowDataIndex]['id']['checkbox'] = false;
+                        self.selectedCount -= 1;
+                    }
+                }
+
+                if (self.selectedCount == self.recordCount) {
+                    $('.ag-custom-checkbox-header input[type="checkbox"]').prop('checked', 1);
+                } else {
+                    $('.ag-custom-checkbox-header input[type="checkbox"]').prop('checked', 0);
+                }
+            })
+
+        // toogle header checkbox
+        $(document)
+            .off("click", ".ag-custom-checkbox-header input[type='checkbox']")
+            .on("click", ".ag-custom-checkbox-header input[type='checkbox']", function() {
+                var i;
+                if ($(this).prop('checked') == true) {
+                    for (i = 0; i < self.rowData.length; i++) {
+                        self.rowData[i]['id']['checkbox'] = true;
+                    }
+                    $('.ag-custom-checkbox input[type="checkbox"]').prop('checked', 1);
+                    self.selectedCount = self.recordCount;
+                } else if ($(this).prop('checked') == false) {
+                    for (i = 0; i < self.rowData.length; i++) {
+                        self.rowData[i]['id']['checkbox'] = false;
+                    }
+                    $('.ag-custom-checkbox input[type="checkbox"]').prop('checked', 0);
+                    self.selectedCount = 0;
+                }
+            })
     }
 
+    // load data from json link
     public loadData() {
         this.dataService.getData().subscribe(data => {
             let json_datas = data.items;
             this.recordCount = json_datas.length;
             for (var index in json_datas) {
                 let row = {};
+                row['id'] = {};
+                row['id']['value'] = index;
+                row['id']['checkbox'] = false;
                 row['thumbnails'] = json_datas[index]['snippet']['thumbnails']['default']['url'];
                 row['publishedAt'] = json_datas[index]['snippet']['publishedAt'];
                 row['title'] = {};
@@ -115,10 +158,18 @@ export class MyGridApplicationComponent implements OnInit {
                 row['description'] = json_datas[index]['snippet']['description'];
                 this.rowData.push(row);
             }
-            this.gridApi.setRowData(this.rowData);
+            setTimeout(() => {
+                this.gridApi.setRowData(this.rowData);
+            }, 100);
+
+            setTimeout(() => {
+                this.autoSizeAll();
+            }, 700);
+
         })
     }
 
+    // video title with link
     public customCellRendererFunc(params) {
         let videoTitle = params.value.value;
         let videoId = params.value.id;
@@ -126,7 +177,7 @@ export class MyGridApplicationComponent implements OnInit {
     }
 
     // custom context to add open in new tab
-    getContextMenuItems(params) {
+    public getContextMenuItems(params) {
         var result: any = [
             "copy",
             "copyWithHeaders",
@@ -146,6 +197,44 @@ export class MyGridApplicationComponent implements OnInit {
             ]
         } 
         return result;
+    }
+
+    // row checkbox
+    public customCheckBox(params) {
+        let Id = "checkbox" + params.value;
+        if (params.value.checkbox) {
+            return `<div class="custom-checkbox custom-control ag-custom-checkbox">
+                        <input id="checkbox${params.value.value}" type="checkbox" class="custom-control-input" checked>
+                        <label for="checkbox${params.value.value}" class="custom-control-label"></label>
+                </div>`;
+        } else {
+            return `<div class="custom-checkbox custom-control ag-custom-checkbox">
+                        <input id="checkbox${params.value.value}" type="checkbox" class="custom-control-input">
+                        <label for="checkbox${params.value.value}" class="custom-control-label"></label>
+                </div>`;
+        }
+    }
+
+    // header checkbox
+    public customHeaderCheckBox() {
+         return `<div class="custom-checkbox custom-control ag-custom-checkbox-header">
+                    <input id="header_checkbox" type="checkbox" class="custom-control-input">
+                    <label for="header_checkbox" class="custom-control-label"></label>
+                </div>`;   
+    }
+
+    // thumbnail image
+    public tuhmbnails(params) {
+        return `<img src="${params.value}" alt="thumbnail">`; 
+    }
+
+    // column resize
+    public autoSizeAll() {
+        var allColumnIds = [];
+        this.gridColumnApi.getAllColumns().forEach(function(column) {
+            allColumnIds.push(column.colId);
+        });
+        this.gridColumnApi.autoSizeColumns(allColumnIds);
     }
 
 }
